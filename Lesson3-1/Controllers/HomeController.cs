4620c7061ee9a23e274aa1b879b54d0561221873
Ivan Zhyhalkin <ivan.zhigalkin@gmail.com>
2022-07-23 21:52:49 +0300

    Add project files.

---
+++ Lesson3-1/Controllers/HomeController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson3_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string firstDigit = "", string secondDigit = "", string operation = "")
+        {
+            var result = "";
+            if (!string.IsNullOrEmpty(operation))
+            {
+                result = Calculate(firstDigit, secondDigit, operation);
+            }
+            return View((object)result);
+        }
+
+        private string Calculate(string firstDigit, string secondDigit, string operation)
+        {
+            if (operation == "*")
+            {
+                return CalculateMulti(firstDigit, secondDigit);
+            }
+            else if (operation == "/")
+            {
+                return CalculateDiv(firstDigit, secondDigit);
+            }
+            else if (operation == "+")
+            {
+                return CalculateSum(firstDigit, secondDigit);
+            }
+            else
+            {
+                return CalculateSubst(firstDigit, secondDigit);
+            }
+        }
+
+        private string CalculateSum(string firstDigit, string secondDigit)
+        {
+            float digit1 = 0;
+            float digit2 = 0;
+            if (float.TryParse(firstDigit, out digit1) && float.TryParse(secondDigit, out digit2))
+            {
+                return (digit1 + digit2).ToString();
+            }
+            return "";
+        }
+        private string CalculateSubst(string firstDigit, string secondDigit)
+        {
+            float digit1 = 0;
+            float digit2 = 0;
+            if (float.TryParse(firstDigit, out digit1) && float.TryParse(secondDigit, out digit2))
+            {
+                return (digit1 - digit2).ToString();
+            }
+            return "";
+        }
+        private string CalculateDiv(string firstDigit, string secondDigit)
+        {
+            float digit1 = 0;
+            float digit2 = 0;
+            if (float.TryParse(firstDigit, out digit1) && float.TryParse(secondDigit, out digit2))
+            {
+                if (digit2 == 0)
+                {
+                    return "NaN";
+                }
+                return (digit1 / digit2).ToString();
+            }
+            return "";
+        }
+        private string CalculateMulti(string firstDigit, string secondDigit)
+        {
+            float digit1 = 0;
+            float digit2 = 0;
+            if (float.TryParse(firstDigit, out digit1) && float.TryParse(secondDigit, out digit2))
+            {
+                return (digit1 * digit2).ToString();
+            }
+            return "";
+        }
+    }
+}
